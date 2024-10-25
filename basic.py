c885aa4e0b4b9fb35bf1239d22c15898a855e263
Tanushwi Singh <156989068+Tanushwi@users.noreply.github.com>
2024-10-25 21:03:45 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,33 @@
+#Q1. WAP to add two numbers
+# a=int(input("Enter first number:"))
+# b=int(input("Enter second number:"))
+# c=a+b
+# print("Sum is:",c);
+
+#Q2. WAP to find area of rectangle
+# l=int(input("Enter length:"))
+# b=int(input("Enter breadth:"))
+# area=l*b
+# print("Area of rectangle is:",area)
+
+#Q3. WAP to find area of circle
+# r=int(input("Enter radius:"))
+# area=3.14*r*r
+# print("Area of circle is:",area)
+
+#Q4. WAP to find circumference of circle
+# r=int(input("Enter radius:"))
+# cir=2*3.14*r
+# print("Circumference of circle is:",cir)
+
+#Q5. WAP to find simple interest
+# p=int(input("Enter principal:"))
+# r=int(input("Enter rate:"))
+# t=int(input("Enter time:"))
+# si=(p*r*t)/100
+# print("Simple interest is:",si)
+
+#Q6. WAP to convert celsius to fahrenheit
+# c=int(input("Enter celsius:"))
+# f=9*c/5+32
+# print("Fahrenheit is:",f)
